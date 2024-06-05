64d2cd4964ac061e4fee89dc1d77f716d008d1e9
eduardotecnologo <eduardotecnologo@hotmail.com.com>
2024-06-04 21:00:32 -0300

    feat: managebean, scoped

---
+++ src/main/java/com/edudeveloper/erp/controller/GestaoEmpresasBean.java
@@ -0,0 +1,22 @@
+package com.edudeveloper.erp.controller;
+
+import java.io.Serializable;
+
+import javax.enterprise.context.ApplicationScoped;
+import javax.inject.Named;
+
+@Named
+@ApplicationScoped
+public class GestaoEmpresasBean implements Serializable{
+	
+	private static final long serialVersionUID = 1L;
+	
+	private static Integer NUMERO = 0;
+	
+	public GestaoEmpresasBean(){
+		NUMERO++;
+	}
+	public Integer getNumero(){
+		return NUMERO;
+	}
+}
